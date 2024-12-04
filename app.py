210a65c84dba5f20c7f1cb6806ed18efee03638b
mdinata <34206130+mdinata@users.noreply.github.com>
2024-12-04 07:40:11 +0700

    change

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import pandas as pd
 import streamlit as st
 st.set_page_config(layout='wide')
 
-with st.sidebar():
+with st.sidebar:
     st.subheader("Colab Coding")
 
 tab1,tab2 = st.tabs(['Home','Project Showcase'])
